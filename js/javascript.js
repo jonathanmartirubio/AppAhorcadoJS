b19bf65db4081ca9403e2499ffe43d66438a9058
Jonathan Martí Rubio <martirubio.dev@gmail.com>
2022-09-21 17:35:56 +0200

    cambio en el fichero

--- js/javascript.js
+++ js/javascript.js
@@ -1,24 +1,64 @@
 var jugador1, jugador2, palabra, letra;
 var CONT_INTENTOS = 6;
-//TODO: Buscar como crear un array con un numero de elementos fijo.
+var PTS_J1 = 0;
+var PTS_J2 = 0;
+let cadauxcorrecta;
+var stringIncorrectas = '';
+
 function EmpezarPartida(){
     /*var formInicial = document.getElementById("form-nombres");
     formInicial.style.display = "block";*/
     window.location.href = 'ahorcado.html';
 }
-function BuscarLetra(){
-    var encontrada = true;
-
-    for (var i = 0; i < palabra.length && encontrada == true; i++) {
+function RellenarCadenaCorrecta(){
+    for(var i = 0; i < palabra.length; i++){
         if(palabra[i] == letra[0]){
             cadauxcorrecta[i] = letra[0];
-        }else{
-            stringIncorrectas += letra[0] + ' ';
-            CONT_INTENTOS--;
-            encontrada = false;
         }
     }
+}
+function RellenarCadenaIncorrecta(){
+    var rellenado = false;
+    for(var i = 0; i < palabra.length && !rellenado; i++){
+        if(palabra[i] != letra[0]){
+            stringIncorrectas += letra[0];
+            rellenado = true;
+        }
+    }
+}
+function BuscarLetra(){
+    var encontrada = false;
+
+    if(palabra.includes(letra[0])){
+        RellenarCadenaCorrecta();
+        encontrada = true;
+    }else{
+        RellenarCadenaIncorrecta();
+        encontrada = false;
+    }
     return encontrada;
+}
+
+function SolucionCorrecta(){
+    var iguales = true;
+
+    if(palabra.length != cadauxcorrecta.length){
+        iguales = false
+    }else{
+        for (let index = 0; index < palabra.length; index++) {
+            if(palabra[index] != cadauxcorrecta[index]){
+                iguales = false;
+                break;
+            }
+        }
+    }
+
+    return iguales;
+}
+
+function ActualizarPuntuaciones(){
+    document.getElementById("pts-j1").value = PTS_J1;
+    document.getElementById("pts-j2").value = PTS_J2;
 }
 
 function RecogerDatos(id){
@@ -35,15 +75,21 @@ function RecogerDatos(id){
             var FormLetraJ2 = document.getElementById("form-letra-j2");
             FormLetraJ2.style.display = "block";
             palabra = document.getElementById("palabra-j1").value;
-            cadaux = new Array(palabra.length);
-            cadaux.fill('-');
+            cadauxcorrecta = new Array(palabra.length);
+            cadauxcorrecta.fill('-');
         break;
         case 'form-letra-j2':
             letra = document.getElementById("letra-j2").value;
             var encontrada = BuscarLetra(); 
             if(encontrada){
-                document.getElementById("letras-correctas").value = cadaux;
+                document.getElementById("letras-correctas").value = cadauxcorrecta.join(' ');
+                    if(SolucionCorrecta()){
+                        PTS_J2++;
+                        ActualizarPuntuaciones();
+                    }
+
             }else{
+                CONT_INTENTOS--;
                 document.getElementById("letras-erroneas").value = stringIncorrectas;
                 document.getElementById("cont-intentos").value = CONT_INTENTOS;
             }
